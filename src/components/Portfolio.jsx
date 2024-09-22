f779cd0200731dfb89bdb409973827b83db8b180
anosike-jayson <kayceeanosike1@gmail.com>
2024-09-22 15:31:26 +0100

    port folio page

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,25 +1,49 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+
 const portfolioData = [
     {
-        image: '/Residential/Garfield/IMG_9157.jpg',
-        description: 'Garfield Multi-family',
-        link: '/selectedworks' 
+        image: '/Residential/Garfield/IMG_9159.jpg',
+        description: 'At ediFel Designs, our main focus is designing with a collaborative approach. We communicate closely with the client, bringing their vision to life. With accredited professionals in LEED, WELL, and Fitwel, as well as Certified Passive House Designers, we offer a wide variety of expertise At ediFel Designs, our main focus is designing with a collaborative approach. We communicate closely with the client, bringing their vision to life. With accredited professionals in LEED, WELL, and Fitwel, as well as Certified Passive House Designers, we offer a wide variety of expertise At ediFel Designs, our main focus is designing with a collaborative approach. We communicate closely with the client, bringing their vision to life. With accredited professionals in LEED, WELL, and Fitwel, as well as Certified Passive House Designers, we offer a wide variety of expertise .',
+        title: 'Garfield Multi-family Project',
+        link: '/selectedworks',
+        relatedImages: [,
+            '/Residential/Garfield/IMG_9167.jpg',
+            '/Residential/Garfield/IMG_9157.jpg',
+            '/Residential/Garfield/IMG_9168.jpg',
+            '/Residential/Garfield/IMG_9172.jpg',
+            '/Residential/Garfield/IMG_9173.jpg',
+            '/Residential/Garfield/IMG_9175.jpg',
+            '/Residential/Garfield/IMG_9176.jpg',
+            '/Residential/Garfield/IMG_9217.jpg',
+            '/Residential/Garfield/IMG_9225.jpg',
+            '/Residential/Garfield/IMG_9227.jpg',
+            '/Residential/Garfield/IMG_9229.jpg',
+            '/Residential/Garfield/IMG_9235.jpg',
+        ]
     },
     {
         image: '/Residential/Glenview/IMG_8982.jpg',
         description: 'Glenview Residence',
-        link: '/selectedworks'
+        title: 'Glenview Project',
+        link: '/selectedworks',
+        relatedImages: [
+            '/Residential/Glenview/IMG_8982.jpg',
+            '/Residential/Glenview/IMG_8992.jpg',
+            '/Residential/Glenview/IMG_8994.jpg',
+        ] 
     },
     {
         image: '/Residential/Green/IMG_9144.jpg',
         description: 'Green Street',
-        link: '/selectedworks'
-    },
-    {
-        image: '/Residential/Lake/image2.png',
-        description: 'Lake Plains',
-        link: '/selectedworks'
+        title: 'Green Project',
+        link: '/selectedworks',
+        relatedImages: [
+            '/Residential/Green/IMG_9144.jpg',
+            '/Residential/Green/IMG_9146.jpg',
+            '/Residential/Green/IMG_9148.jpg',
+
+        ]
     },
 ];
 
@@ -35,13 +59,22 @@ const Portfolio = () => {
                         <div key={index} className="relative group">
                             <img
                                 src={item.image}
-                                alt={`Portfolio ${index + 1}`}
+                                alt={item.title}
                                 className="w-full h-full object-cover rounded-lg transition-transform duration-300"
                             />
                             <div className="absolute inset-0 bg-gradient-to-t from-black via-transparent to-transparent opacity-0 group-hover:opacity-90 transition-opacity duration-300 flex items-end">
                                 <div className="absolute inset-0 p-4 flex flex-col justify-end space-y-2">
-                                    <p className="text-white text-lg">{item.description}</p>
-                                    <Link to={item.link} className="flex items-center text-white text-lg">
+                                    <p className="text-white text-lg">{item.title}</p>
+                                    <Link
+                                        to={item.link}
+                                        state={{ 
+                                            image: item.image, 
+                                            description: item.description, 
+                                            title: item.title,
+                                            relatedImages: item.relatedImages 
+                                        }}
+                                        className="flex items-center text-white text-lg"
+                                    >
                                         <span>Details</span>
                                         <svg className="w-6 h-6 ml-2" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 5l7 7-7 7" />
